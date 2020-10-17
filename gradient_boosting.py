e6f5fd5b4252ad3935c623d8f12ee1320b8e7cfd
Salama <dataubc@gmail.com>
2020-10-17 15:09:43 -0600

    adding docstring to the dataset

--- gradient_boosting.py
+++ gradient_boosting.py
@@ -1,21 +1,40 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
-
 from sklearn.ensemble import GradientBoostingRegressor
-
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
 
-def main():
+def main(path = 'data/ift_data.xlsx', test_size = 0.2 ):
     
-    data = read_data('data/ift_data.xlsx')
+    '''
+    building model tp predict the IFT when a gas is injected into heavy oil emulsion. The model predict the IFT given
+    the type of the gas, water content in the emulsion (fraction) and time elapsed since gas injection
+    
+    inputs:
+    ------
+    - path : path to the input data, excel file that has the following input columns
+       Gas : Categorical variable, the type of the gas injected into the emlusion
+       Water_content : water fraction in the emulsion
+      time_minutes : time elapsed since gas injection
+      IFT: readings of the IFT in  mN/m
+      volume_ratio: volume change since (fraction)
+      
+    - test_size : the size of the data set the will be allocated for testing
+    
+    
+    outputs:
+    ------
+    None, the model print the score of the model in the training and test data set
+    '''
+    
+    data = read_data(path)
     x = data.iloc[:,:3]
     y = data.iloc[:,4]
     
     # splitting the data 
-    X_train, X_test,y_train, y_test = train_test_split(x,y,test_size = 0.2)
+    X_train, X_test,y_train, y_test = train_test_split(x,y,test_size = test_size)
     
     # create a transformer for categorical and numerical features
     

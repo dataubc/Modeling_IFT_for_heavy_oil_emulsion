9df60cdf9986c6aa21e8fdb1caa33892053fe05b
Salama <dataubc@gmail.com>
2022-11-20 12:52:54 -0700

    fixing index.html

--- app.py
+++ app.py
@@ -23,17 +23,20 @@ def predict():
     X_test_new = [x.strip() for x in request.form.values()]
     X_test_new[1] = float(X_test_new[1])
     X_test_new[2] = float(X_test_new[2])
+    X_test_new[3] = float(X_test_new[3])
+
     # final_features = [np.array(int_features)]
     # X_test_new = [x for x in request.form.values()]
 
     # # print(sgd_clf.predict(X_test_new_count))
     #
     ll = [X_test_new]
-    new_data = pd.DataFrame(ll, columns=['Gas', 'Water_content', 'time_minutes'])
+    new_data = pd.DataFrame(ll, columns=['Gas', 'Water_content',
+                                         'viscosity','time_minutes'])
     prediction = model.predict(new_data)[0]
     output = round(prediction, 2)
 
-    return render_template('index.html', prediction_text='Estimated IFT =  {} mN/m'.format(output))
+    return render_template('index.html', prediction_text='Estimated IFT =  {} mN/m'.format( output))
 
 
 if __name__ == "__main__":
